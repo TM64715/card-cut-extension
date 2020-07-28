e2f3f97e35e2e90501d13e0d73bda4568366aadf
Tage <tage.mehta@gmail.com>
2020-07-28 16:51:24 -0400

    Loads Data with Button Press

--- Cards/popup.js
+++ Cards/popup.js
@@ -1,14 +1,41 @@
+// ISSUES
+// Won't capture User selection
+// Won't copy text
+// Btn firing before ev
+
+
 chrome.runtime.onMessage.addListener(notify);
 console.log('Popup.JS running')
-const cardTextArea = document.querySelector('.cardTextArea');
-const btn = document.querySelector('.cardControl');
+const cardTextArea = document.getElementById('cardTextArea');
+const btn = document.getElementById('cardControl');
+var card;
 function notify(message) {
-  var card = message["cardVar"];
+  card = message["cardVar"];
   console.log('Message Received');
   console.log(card);
+
   
-      cardTextArea.value = card;
-      btn.textContent = "Copy To Clipboard";
-  
+		// para.style.visibility = "visible"; 
+  }
+
+function genCard() {
+  cardTextArea.value = card;
+  btn.textContent = "Copy To Clipboard";
 
 }
+
+if (btn.textContent == "Generate Card") {
+    btn.addEventListener("click", genCard)
+
+  }
+else if (btn.textContent == "Copy To Clipboard") {
+    btn.onclick = function(){
+      btn.style.color = "red"
+      var copyText = cardTextArea;
+		  copyText.select();
+		  copyText.setSelectionRange(0, 99999);
+      document.execCommand("copy");
+    }
+  }
+
+  
