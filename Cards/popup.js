458a0ecb662b1459e894264ee2323f066d3219ca
Tage <tage.mehta@gmail.com>
2020-07-28 15:17:55 -0400

    created a hidden element that displays card

--- Cards/popup.js
+++ Cards/popup.js
@@ -2,13 +2,30 @@ chrome.runtime.onMessage.addListener(notify);
 console.log('Popup.JS running')
 const cardTextArea = document.querySelector('.cardTextArea');
 const btn = document.querySelector('.cardControl');
+const storage = document.querySelector('.storage');
 function notify(message) {
   var card = message["cardVar"];
   console.log('Defined Card');
   console.log(card);
-  
-      cardTextArea.value = card;
-      btn.textContent = "Copy To Clipboard";
-  
+  storage.textContent = card;
+ 
 
+}
+const card = storage.textContent;
+if (btn.textContent == "Generate Card") {
+  btn.onclick = function () {
+    cardTextArea.value = card;
+    console.log("Logged Card to Text Box");
+    btn.textContent = "Copy To Clipboard";
+    storage.style.visibility = "visible";
+  }
+}
+else if (btn.textContent == "Copy To Clipboard") {
+    btn.onclick = function() {
+    var copyText = cardTextArea;
+    copyText.select();
+    // copyText.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+    para.style.visibility = "visible";
+  }  
 }
