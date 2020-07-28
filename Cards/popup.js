4f12aaa89f513e160f988789866e3bacb59e73af
Tage <tage.mehta@gmail.com>
2020-07-28 13:07:47 -0400

    Revert "Message Recieved Change"
    This reverts commit 78f257f3100975ad6b4623c3dedf3200a145db38.

--- Cards/popup.js
+++ Cards/popup.js
@@ -4,11 +4,11 @@ const cardTextArea = document.querySelector('.cardTextArea');
 const btn = document.querySelector('.cardControl');
 function notify(message) {
   var card = message["cardVar"];
+  console.log('Message Received');
   console.log(card);
 //   btn.onclick = function() {
       cardTextArea.value = card;
       btn.textContent = "Copy To Clipboard";
-      console.log('Message Received');
   //}
 
 }
