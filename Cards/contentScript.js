9439832dcda45b02c4e54208a60188938ae69905
Tage <tage.mehta@gmail.com>
2020-07-29 15:47:22 -0400

    placeholder text

--- Cards/contentScript.js
+++ Cards/contentScript.js
@@ -217,5 +217,5 @@ chrome.runtime.onMessage.addListener(
 				  "from a content script:" + sender.tab.url :
 				  "from the extension");
 	  if (request.greeting == "hello")
-		sendResponse({"cardVar": cardCompile() });
+		sendResponse({"cardVar":cardCompile()});
 	});
