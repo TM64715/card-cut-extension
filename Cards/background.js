85d9d77dbaac1fde29f79ddf4b4e55de8886c7f4
Tage <tage.mehta@gmail.com>
2020-07-31 17:01:56 -0400

    Revert "Changed Manifest and adding in auth"
    This reverts commit 469c5ed3bdac6d0e7e8f6ea51bde4783b007874d.

--- Cards/background.js
+++ Cards/background.js
@@ -3,7 +3,3 @@ chrome.runtime.onMessage.addListener(notify);
 function notify(message) {
   console.log(message.cardVar);
 }
-
-chrome.identity.getAuthToken({interactive: true}, function(token) {
-  console.log('got the token', token);
-})
