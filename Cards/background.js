469c5ed3bdac6d0e7e8f6ea51bde4783b007874d
Tage <tage.mehta@gmail.com>
2020-07-31 17:01:11 -0400

    Changed Manifest and adding in auth

--- Cards/background.js
+++ Cards/background.js
@@ -3,3 +3,7 @@ chrome.runtime.onMessage.addListener(notify);
 function notify(message) {
   console.log(message.cardVar);
 }
+
+chrome.identity.getAuthToken({interactive: true}, function(token) {
+  console.log('got the token', token);
+})
