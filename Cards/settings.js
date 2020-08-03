c5e19f2f2aeed2e9eef69bb0f2013091c738357e
Tage <tage.mehta@gmail.com>
2020-08-03 16:08:30 -0400

    adding new Page for theme picker

---
+++ Cards/settings.js
@@ -0,0 +1,8 @@
+$(document).ready(main())
+
+function main() {
+    console.log("DOM ready")
+    var color1 = document.getElementById("color1")
+    var heading = document.getElementById("settings")
+        heading.style.textDecoration = "underline"
+}
